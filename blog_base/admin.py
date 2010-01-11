da361711043ec44c4d0d40d0c91f62f7f0dd1ec4
Gustaf Sjöberg <gs@distrop.com>
2010-01-11 15:47:18 +0100

    Correctly set value of input format in admin.

--- blog_base/admin.py
+++ blog_base/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,6 +14,16 @@ class CategoryOptions(admin.ModelAdmin):
 for (_configuration_key, _configuration) in blogs.all():
     if not _configuration.use_generic_admin:
         continue
+
+    # Define a custom form to set initial value of input format field.
+    class Form(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super(Form, self).__init__(*args, **kwargs)
+            self.fields['input_format'].initial = _configuration.markup_format
+
+            class Meta:
+                model = _configuration.model
+
 
     class Admin(admin.ModelAdmin):
         configuration = _configuration
@@ -37,8 +48,6 @@ for (_configuration_key, _configuration) in blogs.all():
         def formfield_for_foreignkey(self, db_field, request, **kwargs):
             if db_field.name == 'author':
                 kwargs['initial'] = request.user.pk
-            if db_field.name == 'input_format':
-                kwargs['initial'] = self.configuration.markup_format
             return super(Admin, self).formfield_for_foreignkey(db_field,
                                                                request,
                                                                **kwargs)
